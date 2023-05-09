d5bce34f74d08f63a54862a0e60bac5891d7492c
Artur Kandaurau <kondaurov.archi@gmail.com>
2023-05-10 00:56:36 +0300

    fix: add minor fixes

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -3,13 +3,13 @@ const path = require('path');
 const folder = path.join(__dirname, 'secret-folder');
 
 fs.readdir(folder, {withFileTypes: true}, (error, files) => {
-    if(error) throw error;
+    if (error) throw error;
     files.forEach(item => {
         if (item.isFile()) {
             const itemPath = path.join(folder, item.name);
             fs.stat(itemPath, (error, params) => {
-                if(error) throw error;
-                console.log(`${path.parse(itemPath).name} - ${path.parse(itemPath).ext.slice(1)} - ${(params.size / 1000).toFixed(3)}kb`);
+                if (error) throw error;
+                console.log(`${path.parse(itemPath).name} - ${path.parse(itemPath).ext.slice(1)} - ${(params.size / 1024).toFixed(3)}kb`);
             });
         }
     });
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,17 +4,17 @@ const curDir = path.join(__dirname, 'files');
 const copyDir = path.join(__dirname, 'files-copy');
 
 fs.mkdir(copyDir, {recursive: true}, (error) => {
-    if(error) throw error;
+    if (error) throw error;
 });
 
 fs.readdir(curDir, {withFileTypes: true}, (error, files) => {
-    if(error) throw error;
+    if (error) throw error;
     files.forEach(item => {
-        if(item.isFile()) {
+        if (item.isFile()) {
             const curFilePath = path.join(curDir, item.name);
             const copyFilePath = path.join(copyDir, item.name);
             fs.copyFile(curFilePath, copyFilePath, (error) => {
-                if(error) throw error;
+                if (error) throw error;
             });
         }
     });
